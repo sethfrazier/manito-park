c94815a50cf1c8497264de8a7d16fa074a096b5f
sfrazier4 <sfrazier4@wisc.edu>
2019-04-02 22:42:08 -0700

    Java

---
+++ js/main2.js
@@ -0,0 +1,304 @@
+// Run showAll function automatically when document loads
+//$( document ).ready(function() {
+//  showAll();
+//});
+
+var refugeBoundary,
+    roads,
+    trailFeatures;
+
+//function createMap(){
+    //create the map with map options
+    var myMap = L.map('mapid',{
+        center: [47.637078, -117.411275],
+        zoom: 14,
+        maxZoom: 18,
+        minZoom: 6, 
+});
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png', {
+            maxZoom: 18
+        }).addTo(myMap);
+
+//move zoomcontrol to topright
+myMap.zoomControl.setPosition('topleft');
+
+var sidebar = L.control.sidebar({
+    autopan:true,
+    closeButton: true,
+    container:'sidebar',
+    position: 'left',
+}).addTo(myMap);
+
+/*
+L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
+    attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
+    minZoom: 1,
+maxZoom: 19}).addTo(myMap);*/
+
+// Function to load the refuge boundary onto the map
+function loadRefugeBoundary() {
+
+    // If the layer is already shown on the map, remove it
+    if (myMap.hasLayer(refugeBoundary)) {
+        myMap.removeLayer(refugeBoundary);
+    }
+
+    // Run the specified sqlQuery from CARTO, return it as a JSON, convert it to a Leaflet GeoJson, and add it to the map with a popup
+
+    // For the data source, enter the URL that goes to the SQL API, including our username and the SQL query
+    $.getJSON("https://sfrazier.carto.com/api/v2/sql?format=GeoJSON&q=SELECT * FROM manitoboundary", function (data) {
+              
+              // Convert the JSON to a Leaflet GeoJson
+              refugeBoundary = L.geoJson(data, {
+
+
+            // Create an initial style for each feature
+            style: function (feature) {
+                return {
+                    color: '#5c8944', // set stroke color
+                    weight: 1.5, // set stroke weight
+                    opacity: 1, // set stroke opacity
+                    fillOpacity: 0.25, // override default fill opacity
+                    fillColor: '#bbe1a4' // set fill color
+                };
+            }
+
+        }).addTo(myMap);
+
+        // Bring the layer to the back of the layer order
+        refugeBoundary.bringToBack();
+    });
+}
+
+
+// Function to load the refuge roads onto the map
+function loadRoads() {
+
+    // If the layer is already shown on the map, remove it
+    if (myMap.hasLayer(roads)) {
+        myMap.removeLayer(roads);
+    }
+
+    // Run the specified sqlQuery from CARTO, return it as a JSON, convert it to a Leaflet GeoJson, and add it to the map with a popup
+
+    // For the data source, enter the URL that goes to the SQL API, including our username and the SQL query
+    $.getJSON('https://sfrazier.carto.com/api/v2/sql?format=GeoJSON&q=SELECT* FROM manitostreets', function (data) {
+
+        // Convert the JSON to a Leaflet GeoJson
+        roads = L.geoJson(data, {
+
+            // Create an initial style for each feature
+            style: function (feature) {
+                return {
+                    color: '#9e559c', // set stroke color
+                    weight: 2.5, // set stroke weight
+                    opacity: 1 // set stroke opacity
+                };
+            },
+
+            // Loop through each feature
+            onEachFeature: function (feature, layer) {
+                
+                // Get the length from the GeoJSON and round it to 2 decimal places
+                var length = parseFloat(feature.properties.route_leng).toFixed(2).toLocaleString();
+
+                // Bind the name and length to a popup
+                layer.bindPopup(feature.properties.route_name + " (" + length + " mi)");                
+
+            }
+
+        }).addTo(myMap);
+
+        // Bring the layer to the back of the layer order
+        //roads.bringToBack();
+    });
+
+}
+
+// Function to load the refuge trail features onto the map
+//function loadTrailFeatures() {
+
+    // If the layer is already shown on the map, remove it
+    if (myMap.hasLayer(trailFeatures)) {
+        myMap.removeLayer(trailFeatures);
+    }
+
+    // Run the specified sqlQuery from CARTO, return it as a JSON, convert it to a Leaflet GeoJson, and add it to the map with a popup
+
+    // For the data source, enter the URL that goes to the SQL API, including our username and the SQL query
+    $.getJSON('https://sfrazier.carto.com/api/v2/sql?format=GeoJSON&q=SELECT* FROM manitofeatures', function (data) {
+
+        // Convert the JSON to a Leaflet GeoJson
+        trailFeatures = L.geoJson(data, {
+
+            // Create a style for the points
+            pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, {
+                    fillColor: '#5d0000',
+                    fillOpacity: 1,
+                    color: '#ffffff',
+                    weight: 0.25,
+                    opacity: 1,
+                    radius: 2.5
+                });
+            },
+
+            // Loop through each feature
+            onEachFeature: function (feature, layer) {
+
+                // Bind the name to a popup
+                layer.bindPopup(feature.properties.feature_type);
+
+            }
+        }).addTo(myMap)
+
+        //}).addTo(trailFeaturesLayerGroup);
+
+        // Turn the layer off by default
+        //map.removeLayer(trailFeaturesLayerGroup);
+    });
+
+//}
+
+
+// Function to load the refuge roads onto the map
+function loadTrails() {
+
+    // If the layer is already shown on the map, remove it
+    if (myMap.hasLayer(roads)) {
+        myMap.removeLayer(roads);
+    }
+
+    // Run the specified sqlQuery from CARTO, return it as a JSON, convert it to a Leaflet GeoJson, and add it to the map with a popup
+
+    // For the data source, enter the URL that goes to the SQL API, including our username and the SQL query
+    $.getJSON('https://sfrazier.carto.com/api/v2/sql?format=GeoJSON&q=SELECT* FROM manitosidewalkstrails', function (data) {
+
+        // Convert the JSON to a Leaflet GeoJson
+        roads = L.geoJson(data, {
+
+            // Create an initial style for each feature
+            style: function (feature) {
+                return {
+                    color: '#808080', // set stroke color
+                    weight: 2.5, // set stroke weight
+                    opacity: 1 // set stroke opacity
+                };
+            },
+
+            // Loop through each feature
+            onEachFeature: function (feature, layer) {
+                
+                // Get the length from the GeoJSON and round it to 2 decimal places
+                var length = parseFloat(feature.properties.route_leng).toFixed(2).toLocaleString();
+
+                // Bind the name and length to a popup
+                layer.bindPopup(feature.properties.route_name + " (" + length + " mi)");                
+
+            }
+
+        }).addTo(myMap);
+
+        // Bring the layer to the back of the layer order
+        //roads.bringToBack();
+    });
+
+}
+
+loadRefugeBoundary();
+loadRoads();
+loadTrails();
+/*
+drawnItems = new L.FeatureGroup();
+        drawControl = new L.Control.Draw({
+        draw: {
+                polygon: false,
+                polyline: false,
+                line: false,
+                marker: true,
+                rectangle: false,
+                circle: false,
+                circlemarker: false,
+            },
+            edit: {
+                featureGroup: drawnItems,
+                edit: false,
+                remove: false
+            },
+            draw: {
+                polygon: {
+                    allowIntersection: false,
+                    showArea: true
+                }
+            }
+        });
+
+ 
+
+myMap.addControl(drawControl);
+myMap.addLayer(drawnItems);*/
+
+
+
+// define client
+          // We have created a empty dataset with private privacy
+          // and we give it insert permission for SQL and select permission for maps
+          // So we can display the geometries added on the map when we refresh
+        const client = new carto.Client({
+            apiKey: '1179d714b3b146401c9e7d6618ba1d043e644f4f',
+            username: 'sfrazier'
+        });
+        let source = new carto.source.SQL(`
+            SELECT * FROM user_input
+        `);
+        
+        let cartoCSS = new carto.style.CartoCSS(`
+            #layer {
+                polygon-fill: red;
+            }`
+        );
+        let cartoLayer = new carto.layer.Layer(source, cartoCSS);
+
+        client.addLayer(cartoLayer);
+
+        client.getLeafletLayer().addTo(myMap);
+    
+// Create Leaflet Draw Control for the draw tools and toolbox
+var drawControl = new L.Control.Draw({
+
+    // Disable drawing of polygons, polylines, rectangles, and circles
+    // Users will only be able to draw markers (points)
+    draw: {
+        polygon: false,
+        polyline: false,
+        rectangle: false,
+        circle: false,
+        circlemarker: false,
+    },
+
+    // Disable editing and deleting points
+    edit: false,
+    remove: false,
+    position: 'topleft'
+});
+
+myMap.addControl(drawControl);
+	
+
+"https://sfrazier.carto.com/api/v2/sql?q=INSERT INTO test_table (column_name, column_name_2, the_geom) VALUES ('this is a string', 11, ST_SetSRID(ST_Point(-110, 43),4326))&api_key={api_key}"
+
+    myMap.on(L.Draw.Event.CREATED, function (e) {
+            let layer = e.layer;
+            myMap.addLayer(layer);
+            let layerAdded = JSON.stringify(layer.toGeoJSON().geometry)
+            
+            // use Fetch API to send request
+            fetch("https://sfrazier.carto.com/api/v2/sql?q=INSERT INTO user_input(the_geom, userinit, userreport, comments) VALUES(st_SetSRID(St_GeomFromGeoJSON('${layerAdded}'), 4326),cff, test2, pwew)&api_key=1179d714b3b146401c9e7d6618ba1d043e644f4f"),
+            method: 'get',mode: 'no-cors'
+            ).then(function(response){
+                console.log(response)
+            }).catch(function(err){
+                console.log(err)
+            })
+    });
--- lib/leaflet-sidebar-v2/README.md
+++ lib/leaflet-sidebar-v2/README.md
@@ -1,42 +1,147 @@
-# sidebar-v2
+# leaflet-sidebar-v2
 
-A responsive sidebar for mapping libraries like [Leaflet](#leaflet) or [OpenLayers](#openlayers-3).
+A responsive sidebar _just_ for [Leaflet](http://leafletjs.com/). A fork of [sidebar-v2](http://turbo87.github.io/sidebar-v2) that only contains the necessary ingredients for use in Leaflet and provides enhanced functionality.
 
-It is more or less a successor of the [leaflet-sidebar](https://github.com/turbo87/leaflet-sidebar/) plugin, thus the `v2` suffix.
+![Demo](doc/leaflet-sidebar-v2.gif)
 
-<a href="https://flattr.com/submit/auto?user_id=turbo&url=https%3A%2F%2Fgithub.com%2FTurbo87%2Fsidebar-v2" target="_blank"><img src="https://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0"></a>
+## Why the Fork?
+- new features based on leaflet.js
+    - JS API for panel modification
+    - "autopan" feature, moving the map content next to the sidebar content
+- only supports leaflet (0.x and 1.x) to maintain a smaller codebase and support more features
+- compatibility with bootstrap (no generic `.sidebar` class)
+- provide a npm package `leaflet-sidebar-v2` with `main` and `style` fields in [`package.json`](package.json)
 
-![Demo](doc/sidebar-v2.gif)
+## Examples
+in `examples` folder, available live at <https://nickpeihl.github.io/leaflet-sidebar-v2/examples>
 
+## Usage
 
-## [Leaflet](http://leafletjs.com/)
+### API
+leaflet-sidebar-v2 provides a simple API to dynamically modify the sidebar. All functions may be chained.
 
-![Sidebar collapsed](doc/leaflet-1.png) ![Sidebar extended](doc/leaflet-2.png)
+#### creation
+The parameters object is fully optional. The default values are shown:
 
-Example code at [`examples/index.html`](examples/index.html) ([Preview](http://turbo87.github.io/sidebar-v2/examples/index.html))
+```js
+var sidebar = L.control.sidebar({
+    autopan: false,       // whether to maintain the centered map point when opening the sidebar
+    closeButton: true,    // whether t add a close button to the panes
+    container: 'sidebar', // the DOM container or #ID of a predefined sidebar container that should be used
+    position: 'left',     // left or right
+}).addTo(map);
+```
 
+#### modification
 
-## [OpenLayers 3](http://openlayers.org/)
+```js
+/* add a new panel */
+var panelContent = {
+    id: 'userinfo',                     // UID, used to access the panel
+    tab: '<i class="fa fa-gear"></i>',  // content can be passed as HTML string,
+    pane: someDomNode.innerHTML,        // DOM elements can be passed, too
+    title: 'Your Profile',              // an optional pane header
+    position: 'bottom'                  // optional vertical alignment, defaults to 'top'
+};
+sidebar.addPanel(panelContent);
 
-![Sidebar collapsed](doc/ol3-1.png) ![Sidebar extended](doc/ol3-2.png)
+/* add an external link */
+sidebar.addPanel({
+    id: 'ghlink',
+    tab: '<i class="fa fa-github"></i>',
+    button: 'https://github.com/nickpeihl/leaflet-sidebar-v2',
+});
 
-Example code at [`examples/ol3.html`](examples/ol3.html) ([Preview](http://turbo87.github.io/sidebar-v2/examples/ol3.html))
+/* add a button with click listener */
+sidebar.addPanel({
+    id: 'click',
+    tab: '<i class="fa fa-info"></i>',
+    button: function (event) { console.log(event); }
+});
 
+/* remove a panel */
+sidebar.removePanel('userinfo');
 
-## [OpenLayers 2](http://openlayers.org/two/)
+/* en- / disable a panel */
+sidebar.disablePanel('userinfo');
+sidebar.enablePanel('userinfo');
+```
 
-![Sidebar collapsed](doc/ol2-1.png) ![Sidebar extended](doc/ol2-2.png)
+#### open / close / show content
+```js
+/* open a panel */
+sidebar.open('userinfo');
 
-Example code at [`examples/ol2.html`](examples/ol2.html) ([Preview](http://turbo87.github.io/sidebar-v2/examples/ol2.html))
+/* close the sidebar */
+sidebar.close();
+```
 
+#### remove sidebar
 
-## [Google Maps](https://developers.google.com/maps/)
+```js
+/* remove the sidebar (keeping the sidebar container) */
+sidebar.remove();
+sidebar.removeFrom(map); // leaflet 0.x
 
-![Sidebar collapsed](doc/gmaps-1.png) ![Sidebar extended](doc/gmaps-2.png)
+/* to clear the sidebar state, remove the container reference */
+sidebar._container = null
+```
 
-Example code at [`examples/gmaps.html`](examples/gmaps.html) ([Preview](http://turbo87.github.io/sidebar-v2/examples/gmaps.html))
+### markup
+If you use the sidebar with static content only, you can predefine content in HTML:
+
+```html
+<div id="sidebar" class="leaflet-sidebar collapsed">
+    <!-- Nav tabs -->
+    <div class="leaflet-sidebar-tabs">
+        <ul role="tablist"> <!-- top aligned tabs -->
+            <li><a href="#home" role="tab"><i class="fa fa-bars"></i></a></li>
+            <li class="disabled"><a href="#messages" role="tab"><i class="fa fa-envelope"></i></a></li>
+            <li><a href="#profile" role="tab"><i class="fa fa-user"></i></a></li>
+        </ul>
+
+        <ul role="tablist"> <!-- bottom aligned tabs -->
+            <li><a href="#settings" role="tab"><i class="fa fa-gear"></i></a></li>
+        </ul>
+    </div>
+
+    <!-- Tab panes -->
+    <div class="leaflet-sidebar-content">
+        <div class="leaflet-sidebar-pane" id="home">
+            <h1 class="leaflet-sidebar-header">
+                sidebar-v2
+                <div class="leaflet-sidebar-close"><i class="fa fa-caret-left"></i></div>
+            </h1>
+            <p>A responsive sidebar for mapping libraries</p>
+        </div>
+
+        <div class="leaflet-sidebar-pane" id="messages">
+            <h1 class="leaflet-sidebar-header">Messages<div class="leaflet-sidebar-close"><i class="fa fa-caret-left"></i></div></h1>
+        </div>
+
+        <div class="leaflet-sidebar-pane" id="profile">
+            <h1 class="leaflet-sidebar-header">Profile<div class="leaflet-sidebar-close"><i class="fa fa-caret-left"></i></div></h1>
+        </div>
+    </div>
+</div>
+```
+
+You still need to initialize the sidebar (see API.creation)
+
+### Events
+
+The sidebar fires 3 types of events:
+`opening`, `closing`, and `content`.
+The latter has a payload including the id of the activated content div.
+
+You can listen for them like this:
+```js
+sidebar.on('content', function(e) {
+    // e.id contains the id of the opened panel
+})
+```
 
 
 ## License
 
-sidebar-v2 is free software, and may be redistributed under the [MIT license](LICENSE).
+leaflet-sidebar-v2 is free software, and may be redistributed under the [MIT license](LICENSE).
